6c4b2a972557e2928066a1776de8c069c872cfe8
bellinux <bellinuxcom@gmail.com>
2025-07-29 10:42:55 -0400

    Add files via upload

---
+++ protobject-basic/config.js
@@ -0,0 +1,15 @@
+Protobject.setProduction(true)
+Protobject.initialize(
+	[
+		{ 
+			name: "Servo",
+			page: "arduino.html",
+		},
+		{ 
+			name: "Knob",
+			page: "index.html",
+			main: true,
+		}
+	]);
+
+ 
